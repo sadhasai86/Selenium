7d21d77f83bd71049753136c36a164b3c754ef38
sadhasai86 <sadhasai86@gmail.com>
2017-11-04 23:09:31 +0530

    file added

--- src/test/java/test/selenium/NewTest.java
+++ src/test/java/test/selenium/NewTest.java
@@ -1,16 +1,16 @@
 package test.selenium;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-public class Amazon {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		FirefoxDriver driver = new FirefoxDriver();
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  FirefoxDriver driver = new FirefoxDriver();
 		driver.get("https://www.amazon.in/");
 		
 		System.out.println(driver.getTitle());
-	}
 
+  }
 }
